e4bf67501e332bd0c659f8c6776d559ee3c31e74
mathisarends <140075923+mathisarends@users.noreply.github.com>
2023-11-18 21:10:46 +0100

    implemented all routes with dummy data

--- server.py
+++ server.py
@@ -1,17 +1,52 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# GET-Route
+# Zombie-Server Schnittstellen
+
+# POST /z/init
+@app.route('/z/init', methods=['POST'])
+def zombie_init(): 
+    #data = request.get_json()
+
+    # Hier können die empfangenen Daten verarbeitet werden...
+    return jsonify({ "message": "Zombie Initialisierung erfolgreich!" }), 200 #Status 200 = OK
+
+
+# GET /z/update
+@app.route('/z/update', methods=['GET'])
+def zombie_update():
+    #data = request.get_json()
+
+    # Hier können die empfangenen Daten verarbeitet werden...
+    return jsonify({"speed": 0.5, "direction": 0.7, "rotation": 0.2}), 200 #Status 200 = OK
+
+
+# Human Schnittstellen
+
+# POST /h/init
+@app.route('/h/init', methods=['POST'])
+def human_init():
+    data = request.get_json()
+
+    # Hier können die empfangenen Daten verarbeitet werden...
+    return jsonify({"message": "Human Initialization Successful"}), 200 #Status 200 = OK
+
+
+# GET /h/update
+@app.route('/h/update', methods=['GET'])
+def human_update():
+    #data = request.get_json()
+    # Hier können die empfangenen Daten verarbeitet werden...
+
+    return jsonify({"speed": 0.8, "direction": 0.1}), 200 #Status 200 = OK
+
+
+# GET-Route MAIN
 @app.route('/', methods=['GET'])
 def get_route():
     return 'GET Route: Hello, World!'
 
-# POST-Route
-@app.route('/', methods=['POST'])
-def post_route():
-    data = request.get_data(as_text=True)
-    return f'POST Route: Received data - {data}'
 
 if __name__ == '__main__':
     # Starte den Webserver auf Port 5000 (Standardport für Flask)
